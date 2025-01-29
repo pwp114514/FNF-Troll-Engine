6a4e6d271b696e4237d014246f2db3e9565e6691
fnfpwp <117086223+pwp114514@users.noreply.github.com>
2025-01-29 14:30:09 +0800

    Update FlxTransitionableState.hx

--- source/flixel/addons/transition/FlxTransitionableState.hx
+++ source/flixel/addons/transition/FlxTransitionableState.hx
@@ -63,7 +63,7 @@ class FlxTransitionableState extends FlxState
 		transitionIn();
 	}
 
-	override public function switchTo(nextState:FlxState):Bool
+	override public function startOutro(nextState:FlxState):Bool
 	{
 		// If you get an exception here it's probably due to Flixel calling this function using reflection
 		if (!hasTransOut)
